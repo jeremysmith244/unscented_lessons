e4f60f63a284feae058a5d32052b18ed8733acbd
root <jeremysmith244@gmail.com>
2019-12-27 11:52:49 -0800

    third excercise

---
+++ point_pred/build/CMakeFiles/aug.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/aug.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ point_pred/build/CMakeFiles/point_pred.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/point_pred.dir/point_pred.cpp.o"
+  "CMakeFiles/point_pred.dir/ukf.cpp.o"
+  "point_pred.pdb"
+  "point_pred"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/point_pred.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
